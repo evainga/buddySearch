9bc2d57321dfffa987c788fcc75f29bfa0a0c37d
Eva Goettert <eva.goettert@idealo.de>
2019-11-10 13:32:44 +0100

    Fix index.js

--- buddy-search/index.js
+++ buddy-search/index.js
@@ -10,23 +10,15 @@ app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+
 app.get('/boulderer/all', async (req, res) => {
   const boulderers = await BouldererService.findAll()
   res.render('boulderer', { boulderers })
-})
-
-app.get('/buddy-search/all', async (req, res) => {
-  const buddySearches = await BuddySearchService.findAll()
-  res.render('buddy-search', { buddySearches })
-})
-
-app.get('/location/all', async (req, res) => {
-  const locations = await LocationService.findAll()
-  res.render('location', { locations })
 })
 
 app.get('/boulderer/:id', async (req, res) => {
@@ -35,10 +27,21 @@ app.get('/boulderer/:id', async (req, res) => {
   res.send(boulderer)
 })
 
-app.get('/location/:id', async (req, res) => {
+app.post('/boulderer', async (req, res) => {
+  const boulderer = await BouldererService.add(req.body)
+  res.send(boulderer)
+})
+
+app.delete('/boulderer/:id', async (req, res) => {
   const id = req.params.id
-  const location = await LocationService.find(id)
-  res.send(location)
+  await BouldererService.del(id)
+  res.send('ok')
+})
+
+
+app.get('/buddy-search/all', async (req, res) => {
+  const buddySearches = await BuddySearchService.findAll()
+  res.render('buddy-search', { buddySearches })
 })
 
 app.get('/buddy-search/:id', async (req, res) => {
@@ -47,9 +50,27 @@ app.get('/buddy-search/:id', async (req, res) => {
   res.send(buddySearch)
 })
 
-app.post('/boulderer', async (req, res) => {
-  const boulderer = await BouldererService.add(req.body)
-  res.send(boulderer)
+app.post('/buddy-search', async (req, res) => {
+  const buddySearch = await BuddySearchService.add(req.body)
+  res.send(buddySearch)
+})
+
+app.delete('/buddy-search/:id', async (req, res) => {
+  const id = req.params.id
+  await BuddySearchService.del(id)
+  res.send('ok')
+})
+
+
+app.get('/location/all', async (req, res) => {
+  const locations = await LocationService.findAll()
+  res.render('location', { locations })
+})
+
+app.get('/location/:id', async (req, res) => {
+  const id = req.params.id
+  const location = await LocationService.find(id)
+  res.send(location)
 })
 
 app.post('/location', async (req, res) => {
@@ -57,28 +78,12 @@ app.post('/location', async (req, res) => {
   res.send(location)
 })
 
-app.post('/buddy-search', async (req, res) => {
-  const buddySearch = await BuddySearchService.add(req.body)
-  res.send(buddySearch)
-})
-
-app.delete('boulderer/:id', async (req, res) => {
-  const id = req.params.id
-  await BouldererService.del(id)
-  res.send('ok')
-})
-
-app.delete('location/:id', async (req, res) => {
+app.delete('/location/:id', async (req, res) => {
   const id = req.params.id
   await LocationService.del(id)
   res.send('ok')
 })
 
-app.delete('buddy-search/:id', async (req, res) => {
-  const id = req.params.id
-  await BuddySearchService.del(id)
-  res.send('ok')
-})
 
 app.listen(3000, () => {
   console.log('listening')
