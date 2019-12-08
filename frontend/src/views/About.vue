edfa773081dfa4890d038051b2c0cb4770e77239
evainga <ego87@web.de>
2019-12-08 12:55:47 +0100

    check if automatic deployment works

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>Welcome to Boulder Buddy!</h1>
+    <h1>Welcome to Boulder Buddy! test</h1>
       <h2>This is your app to find a buddy for bouldering and climbing.</h2>
   </div>
 </template>
